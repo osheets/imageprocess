6d198def79a72004d54db08cabaf8b4ee523cdd7
Stevie Barot <steviebarot@gmail.com>
2023-04-16 22:44:49 +0800

    Complete blog post 1

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("mouseenter", function() {
+  coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
@@ -31,27 +31,27 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("mouseout", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
+// for (i = 0; i < coll.length; i++) {
+//   coll[i].addEventListener("mouseout", function() {
+//     this.classList.toggle("active");
+//     var content = this.nextElementSibling;
+//     if (content.style.display === "block") {
+//       content.style.display = "none";
+//     } else {
+//       content.style.display = "block";
+//     }
+//   });
+// }
 
 
 // When the user scrolls the page, execute myFunction
 // document.getElementsByClassName("container").addEventListener("scroll", myFunction);
 
-function myFunction() {
+function scrollFunction() {
   const winScroll = document.getElementById("container").scrollTop;
   const height = document.getElementById("container").scrollHeight - document.documentElement.clientHeight;
   const scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
 
-document.getElementById("container").onscroll = function() {myFunction()};
+document.getElementById("container").onscroll = function() {scrollFunction()};
